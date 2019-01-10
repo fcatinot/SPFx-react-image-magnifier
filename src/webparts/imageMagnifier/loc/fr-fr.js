f5b2f80e9beca56ff665c7ff39dd7bd21c6d9c00
Franck <f.catinot@gmail.com>
2019-01-10 12:14:21 +0100

    add loc fr

---
+++ src/webparts/imageMagnifier/loc/fr-fr.js
@@ -0,0 +1,16 @@
+define([], function() {
+  return {
+    PropertyPaneDescription: "Configurer la WebPart",
+    BasicGroupName: "",
+    DescriptionFieldLabel: "Description",
+    SmallImgUrlFieldLabel: "Url petite image",
+    SmallImgWidthFieldLabel: "Largeur petite image (px)",
+    SmallImgHeightFieldLabel: "Hauteur petite image (px)",
+    LargeImgUrlFieldLabel: "Url grande image",
+    LargeImgWidthFieldLabel: "Largeur grande image (px)",
+    LargeImgHeightFieldLabel: "Hauteur grande image (px)",
+    CursorOffsetXFieldLabel: "Le décallage du zoom par rapport au curseur X (px)",
+    CursorOffsetYFieldLabel: "Le décallage du zoom par rapport au curseur Y (px)",
+    SizeFieldLabel: "Taille de la lentille (px)"
+  };
+});
